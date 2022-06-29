704e0927b720ce98a961dda1edba06ab2108dc76
Rafael Sangali <rafaelsangali@outlook.com>
2022-06-29 15:44:16 -0300

    instalando e configurando pastas

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer:{
+
+  },
+})
+
+export type RootStore = ReturnType<typeof store.getState>;
+
+export default store;
