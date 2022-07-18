f77247430bb5ad76bfff7a744a16b81f3339c28f
Rafael Sangali <rafaelsangali@outlook.com>
2022-07-18 14:11:08 -0300

    criando setup estudos next.js

--- studying-context-axios/src/Routes.tsx
+++ studying-context-axios/src/Routes.tsx
@@ -2,7 +2,6 @@ import { Routes, Route , BrowserRouter} from "react-router-dom"
 import Blog from "./components/Blog/Blog"
 import Contato from "./pages/Contato/Contato"
 import Home from "./pages/Home/Home"
-import Teste from "./pages/Teste"
 
 export default function Wrapper(){
 return (
@@ -11,7 +10,6 @@ return (
         <Route path="/" element={<Home />}/>
         <Route path="/contato" element={<Contato />}/>
         <Route path="/blog" element={<Blog />}/>
-        <Route path="/teste" element={<Teste />}/>
     </Routes>
     </BrowserRouter>
 )
